91899f1451ae394a00fb13e836c4690b8926db72
Matthew Lowden <matthew.lowden@ijot.nl>
2022-11-14 17:34:01 +0100

    Add JSDoc. Factor if(verbose) to a seperate function.

--- src/waitForApplication.mjs
+++ src/waitForApplication.mjs
@@ -48,27 +48,54 @@ const argv = yargs(hideBin(process.argv))
     .alias('help', 'h')
     .argv;
 
-async function waitForApplication() {
-    let timeout = Date.now() + argv.timeout * 1000;
+/**
+ * Wait for a service to be up and ready.
+ * An HTTP response of 400 or more is assumed to be a temporary failure.
+ * An HTTP response of 399 ot lower is assumed to be ready.
+ * 
+ * @param: {Object} parameters - Parameters from the command line.
+ * @param: {string} parameters.application - The URL of the service e.g. https://myapplication.mydomain.com
+ * @param: {boolean} parameters.verbose - Run with verbose logging.parameters
+ * @param: {number} parameters.retry - Retry interval (seconds)
+ * @param: {number} parameters.timeout - Timeout interval (seconds)
+ * 
+ */
+async function waitForApplication(parameters) {
+    logIfVerboseLoggingEnabled(parameters.verbose, 'Waiting for: ' + parameters.application);
+    logIfVerboseLoggingEnabled(parameters.verbose, 'Retry interval (seconds): ' + parameters.retry);
+    logIfVerboseLoggingEnabled(parameters.verbose, 'Timeout interval (seconds): ' + parameters.timeout);
+
+    let timeout = Date.now() + parameters.timeout * 1000;
     let response;
     do {
-        response = await fetch(argv.application);
+        response = await fetch(parameters.application);
         if (response.status >= 400) {
-            if (argv.verbose) {
-                console.log('Application: ' + argv.application + ' is not ready. Retrying in ' + argv.retry + 's.');
-            }
+            logIfVerboseLoggingEnabled(parameters.verbose, 'Application: ' + parameters.application + ' is not ready. Retrying in ' + parameters.retry + 's.');
             if (Date.now() > timeout) {
-                console.error('Application did not become ready within the timeout period (' + argv.timeout + 's). Aborting.');
+                console.error('Application did not become ready within the timeout period (' + parameters.timeout + 's). Aborting.');
                 process.exitCode = 1;
             } else {
-                await setTimeout(argv.retry * 1000);
+                await setTimeout(parameters.retry * 1000);
             }
         } else {
-            if (argv.verbose) {
-                console.log('Application: ' + argv.application + ' is ready');
-            }
+            logIfVerboseLoggingEnabled(parameters.verbose, 'Application: ' + parameters.application + ' is ready');
         }
     } while (response.status >= 400 && !process.exitCode);
 }
 
-waitForApplication();
+/**
+ * Logs a message to the console, if verbose logging is enabled.
+ * 
+ * @param: {boolean} verbose - Verbose logging enabled if true.
+ * @param: {*} message - The message to be logged.
+ * 
+ */
+function logIfVerboseLoggingEnabled(verbose, message) {
+    if (verbose) {
+        console.log(message);
+    }
+}
+
+waitForApplication(argv);
+
+export default waitForApplication;
